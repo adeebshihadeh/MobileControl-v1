88d55622440f9f9a7fe6b02b750055f6bb695226
quillford <adeebshihadeh@gmail.com>
2015-08-01 16:03:31 -0700

    menu tab system

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+setup();
+
 function testConnection(){
+    console.log("testing connection...");
+    $("#test_status").text("testing...");
+    $("#test_status").css("color", "black");
+    
     $.ajax({
         url:  "http://"+$("#ip_field").val()+"/api/version",
         headers: {"X-Api-Key": $("#apikey_field").val()},
         method: "GET",
         timeout: 10000
     }).done(function() {
-        return true;
+        $("#test_status").text("success");
+        $("#test_status").css("color", "green"); 
     }).fail(function(){
-        return false;
+        $("#test_status").text("connection failed");
+        $("#test_status").css("color", "red");
     });
 }
 
@@ -17,6 +25,7 @@ function setup(){
         switchView("first_time_setup");
     }else {
         switchView("main");
+        switchTab("info_tab");
     }
 }
 
@@ -31,25 +40,46 @@ function switchView(view) {
     $("#"+view).show();
 }
 
+function switchTab(tab){
+    // deselect all tabs
+    $("#info_tab_btn").removeClass("menu-tab-selected");
+    $("#control_tab_btn").removeClass("menu-tab-selected");
+    $("#files_tab_btn").removeClass("menu-tab-selected");
+    $("#settings_tab_btn").removeClass("menu-tab-selected");
+    
+    // hide all the contents of the tabs
+    $("#info_tab").hide();
+    $("#control_tab").hide();
+    $("#files_tab").hide();
+    $("#settings_tab").hide();
+    
+    // select the desired tab
+    $("#"+tab+"_btn").addClass("menu-tab-selected");
+    $("#"+tab).show();
+}
+
 // ------------- button click events -------------
 // first time setup buttons
 $("#test_connection_btn").click(function() {
-    $("#test_status").text("testing...");
-    $("#test_status").css("color", "black");
-    if(testConnection()){
-        $("#test_status").text("success");
-        $("#test_status").css("color", "green"); 
-    }else {
-        $("#test_status").text("connection failed");
-        $("#test_status").css("color", "red");
-    }
+    console.log("clicked!");
+    testConnection();
 });
-
 $("#next_screen_btn").click(function () {
+    localStorage.setItem("ip_address", $("#ip_field").val());
+    localStorage.setItem("apikey", $("#apikey_field").val());
     switchView("main");
-    localStorage.setItem("ip_address", $("ip_field").val());
-    localStorage.setItem("apikey", $("apikey_field").val());
 });
 
-
-setup();
+// tab menu buttons
+$("#info_tab_btn").click(function() {
+    switchTab("info_tab");
+});
+$("#control_tab_btn").click(function() {
+    switchTab("control_tab");
+});
+$("#files_tab_btn").click(function() {
+    switchTab("files_tab");
+});
+$("#settings_tab_btn").click(function() {
+    switchTab("settings_tab");
+});
